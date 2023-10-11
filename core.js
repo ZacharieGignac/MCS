cc8a06d0097d87636cc44854c9f8262ebe629e0b
Zacharie Gignac <zacharie.gignac@gmail.com>
2023-10-11 13:06:30 -0400

    Added scheduled standby #12

--- core.js
+++ core.js
@@ -36,6 +36,15 @@ function debug(level, text) {
   }
 }
 
+function schedule(time, action) {
+  let [alarmH, alarmM] = time.split(':');
+  let now = new Date();
+  now = now.getHours() * 3600 + now.getMinutes() * 60 + now.getSeconds();
+  let difference = parseInt(alarmH) * 3600 + parseInt(alarmM) * 60 - now;
+  if (difference <= 0) difference += 24 * 3600;
+  return setTimeout(action, difference * 1000);
+}
+
 
 class Performance {
   constructor() {
@@ -507,8 +516,8 @@ class Core {
       });
     });
 
-    delete(zapi.system.systemReport.codecConfig);
-    delete(zapi.system.systemReport.codecStatus);
+    delete (zapi.system.systemReport.codecConfig);
+    delete (zapi.system.systemReport.codecStatus);
 
 
   }
@@ -641,6 +650,20 @@ class Core {
         Mode: 'Off'
       });
     }
+
+    this.scheduleStandby = () => {
+      schedule(config.system.forceStandbyTime, () => {
+        this.scenarios.enableScenario(config.system.onStandby.enableScenario);
+        this.scheduleStandby();
+      });
+    }
+    this.scheduleStandby();
+
+    //Setup auto standby
+    if (config.system.forceStandby) {
+
+    }
+
 
     //Setup devices
     this.devicesManager = new DevicesManager();

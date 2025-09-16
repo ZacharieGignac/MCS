03c865078ca655395bf2305e172c6c5e6aec7a67
Zacharie Gignac <zacharie.gignac@gmail.com>
2025-09-16 14:36:25 -0400

    Handle errors in grid layout activation
    Wrapped the grid layout command in a try-catch block and added a catch handler to prevent unhandled promise rejections when setting the video layout. Also bumped module version to 1.0.1.

--- modules/mod_autogrid.js
+++ modules/mod_autogrid.js
@@ -5,7 +5,7 @@ export var Manifest = {
   fileName: 'mod_autogrid',  //Nom du fichier
   id: 'autogrid',  //Identification unique
   friendlyName: 'Auto Grid Layout On Call', //Nom familier
-  version: '1.0.0', //Version
+  version: '1.0.1', //Version
   description: `Utilise automatiquement le mode "grille" lors d'un appel` //Description
 };
 
@@ -13,11 +13,15 @@ export class Module {
   constructor() {
 
   }
-  
+
   start() {
     xapi.Event.CallSuccessful.on(call => {
       setTimeout(() => {
-        xapi.Command.Video.Layout.SetLayout({ LayoutName: 'Grid' });
+        try {
+          xapi.Command.Video.Layout.SetLayout({ LayoutName: 'Grid' })
+          .catch(() => { });
+        }
+        catch { }
       }, 10);
     });
   }
